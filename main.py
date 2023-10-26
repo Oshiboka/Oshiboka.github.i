c094635c9f045d103432ee161020cd95553cd45f
Oshiboka <cssyan123@gmail.com>
2023-10-26 15:02:05 +0300

    Initiall commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import telebot
+
+# Initialize bot with your Telegram Bot Token
+bot = telebot.TeleBot('6899909977:AAF4PUn1mvt2x9DWXbg03OEtLye6nT-VD10')
+
+# Define a handler for the /start command
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, 'Hello, welcome to my bot!')
+
+# Define a handler for text messages
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+# Start the bot
+bot.polling()
